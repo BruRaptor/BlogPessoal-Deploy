239c2140083898eae7a250be4618446d4f1cbf8d
Bruno Ribeiro <85457639+BruRaptor@users.noreply.github.com>
2022-02-04 12:29:49 -0300

    @BruRaptor [Blog Pessoal Deploy]

---
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -0,0 +1,99 @@
+package org.generation.blogPessoal.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+	
+	//Primeiro Método Construtor - Com atributos
+	
+    public Usuario(long id, String nome,String usuario,String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+    
+  //Primeiro Método Construtor - Sem atributos
+    public Usuario() { }
+    
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String nome;
+    
+    @Schema(example = "email@mail.com.br")
+    @NotNull (message = "O atributo Usuário é Obrigatótio!")
+    @Email(message = "O atributo Usuário deve ser um email válido!")
+    @Size(min = 5, max = 100)
+    private String usuario;
+    
+    @NotNull
+    @Size(min = 5, max =100)
+    private String senha;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagens = new ArrayList<>();
+
+    public List<Postagem> getPostagens() {
+        return this.postagens;
+    }
+
+    public void setPostagens(List<Postagem> postagens) {
+        this.postagens = postagens;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return this.usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
---
+++ src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -0,0 +1,81 @@
+package org.generation.blogPessoal.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.generation.blogPessoal.model.UserLogin;
+import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    
+    @Autowired
+    private UsuarioRepository repository;
+
+    public ResponseEntity<Usuario> CadastrarUsuario(Usuario usuario){
+
+        Optional<Usuario> optional = repository.findByUsuario(usuario.getUsuario());
+        if(optional.isPresent()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        String senhaEncoder = encoder.encode(usuario.getSenha());
+
+        usuario.setSenha(senhaEncoder);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+    }
+
+    public Optional<UserLogin> UpdateUsuario(Optional<UserLogin> user){
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+
+        if(usuario.equals(user)){
+            if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())){
+              
+                String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+                byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+
+                String authHeader = "Basic " + new String(encodedAuth);
+
+                user.get().setToken(authHeader);
+                user.get().setNome(usuario.get().getNome());
+
+                return user;
+            }
+         }
+        return null;
+    }
+    
+    public Optional<UserLogin> Logar(Optional<UserLogin> user){
+        
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+
+        if(usuario.isPresent()){
+            if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())){
+              
+                String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+                byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+
+                String authHeader = "Basic " + new String(encodedAuth);
+
+                user.get().setToken(authHeader);
+                user.get().setNome(usuario.get().getNome());
+
+                return user;
+            }    
+        }
+        return null;
+    }
+}
